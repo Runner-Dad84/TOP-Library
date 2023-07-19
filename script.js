c09fcb94d7442c83f61e7eaedd371ba50225a4aa
Runner-Dad84 <vanhoogie@gmail.com>
2023-07-18 22:08:21 -0400

    Html form and form button with onclick function. Add fuction to make form appear.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let library = ["Peter Pan", "Treasure Island", "Running with the Buffalos"];
+//See Display form
+let count = 0;
 
 
 //constructor
@@ -17,6 +19,22 @@ function addBook () {
 
 };
 
+let displayForm = function () {
+    let form = document.getElementById("form");
+    let button = document.getElementById("formButton");
+    if (count % 2) {
+        console.log(count);
+        form.style.display="none"
+        count+=1;
+    } else {
+        console.log(count);
+        form.style.display="block"
+        count+=1;
+    };
+    
+}
+
+//adds array item to DOM and to document on shelf
 function displayBook (){
     for (i = 0; i < library.length; i++){
         let node = document.createElement("div");
